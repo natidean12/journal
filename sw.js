8500c488af6d888e75283659e92f2bf1b96d2f9b
jounal <162047414+natidean12@users.noreply.github.com>
2026-01-25 03:39:06 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = "reading-journal-v4"; // Mudei para v4 para o iPad forçar a atualização
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",     // Garante que a tela do botão seja salva!
+  "./livros.html",
+  "./roleta.html",
+  "./manifest.json",
+  "./icons/capas.png", 
+  "./offline.html" 
+];
